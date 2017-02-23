b5c6e1d6c03af9a9035f7d02498e5bcfac69a79c
Bryan Raul Lizana De La O <Bryan Raul Lizana De La O>
2017-02-22 22:32:20 -0500

    Finish

--- views/alumno/detail.php
+++ views/alumno/detail.php
@@ -25,8 +25,8 @@ $data = $data_complete[0];
 <h2>Detalles del Usuario</h2>
 <form class="pure-form pure-form-aligned" action="" method="POST">
 <?php include('../templates/form/user-form.php'); ?>
-<hr>
-        <span>Cambiar password::</span><br>
+<!--<hr>-->
+        <!--<span>Cambiar password::</span><br>
         <div class="pure-control-group">
             <label for="password">Nuevo Password</label>
             <input id="new_password" type="password" placeholder="New Password">
@@ -38,8 +38,83 @@ $data = $data_complete[0];
         <div class="pure-control-group">
             <label for="password">Password Actual</label>
             <input id="password" type="password" placeholder="Password">
-        </div>
+        </div>-->
 </form>
+
+<?php  ///weeek
+            $ddate = date('Y-m-d');
+            $date = new DateTime($ddate);
+            $week = $date->format("W");
+            // echo "Weeknummer: $week";
+            $add_sem = (is_numeric($_GET['week'])) ? $_GET['week'] : '0' ;
+            $week_number = (date('W',strtotime( $add_sem." week") ) ) ;// (is_numeric($_GET['week'])  && $_GET['week'] != '0')? $_GET['week'] : $week ;
+            $year = date('Y');
+            for($day=1; $day<=5; $day++)
+            {
+                $fechas[] = "'".date('Y-m-d', strtotime($year."W".$week_number.$day))."'" ;
+            }
+
+ ?>
+
+<h2>Asistencias</h2>
+<h3>Alumno:XX</h3>
+<h4>Fecha: Del Lunes- <?php echo $fechas[0] ?>  al   Viernes-<?php echo $fechas[count($fechas)-1] ?> </h4>
+<a href="?week=<?php echo (  $add_sem - 1 ) ?>"> << Semena anterior</a>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="?week=<?php echo ( $add_sem +  1 ) ?>">Semena siguiente >>  </a>
+<br>
+<br>
+
+
+<table class="pure-table">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Docente</th>
+            <th>Curso</th>
+            <th>Lunes</th>
+            <th>Martes</th>
+            <th>Miércoles</th>
+            <th>Jueves</th>
+            <th>Viernes</th>
+        </tr>
+    </thead>
+
+    <tbody>
+
+            <?php 
+            $n_i = 1;
+            foreach ($data_complete as $alumno_control) {
+            $alumno_asistencia =  new AlumnoAsistencias();
+            $alumno_asistencia->id_alumno_control = $alumno_control['id_alumno_control'];
+
+            $alumno_asistencia->string_ids = implode(',',$fechas);
+            $asistencia = $alumno_asistencia->list_alumno_control_asistencia();
+
+                $curso = $alumno_control['description']
+                ?> <tr class="pure-table-odd">
+                    <td><?php echo $n_i ; $n_i++?></td>
+                    <td><?php echo  $alumno_control['last_name_docente'] ?> </td>                     
+                    <td><?php echo $curso ?> </td> 
+                    
+                <?php $no = 0; ?>
+                <?php  foreach ($asistencia as $dia) {    ?> 
+                            <?php if ($no == 0) :  ?> 
+                                <?php foreach ($fechas as $fecha): ?>
+                                <?php if ("'".$dia['fecha']."'" != $fecha) :  ?> 
+                                    <td></td>
+                                <?php  else: $no = 1; break ?>
+                                <?php  endif;  ?>
+                                <?php endforeach ?>
+                            <?php  endif;  ?>
+                           <td><?php echo  $dia['asistencia'] ?></td>
+                <?php }            
+            ?> </tr>  <?php
+            }
+        ?>
+    </tbody>
+</table>
 
 <hr>    
 <h2>Notas</h2>
@@ -88,26 +163,17 @@ function list_nota_alumno_($temporada_class,$id_alumno_control)
     <td>Promedio</td> 
       <?php          
 } 
+$n_i = 1;
 foreach ($data_complete as $alumno_control) {
     ?> 
             <tr class="pure-table-odd">
-            <td>1</td>
+            <td><?php echo $n_i ; $n_i++?></td>
     
     <?php
        //list notas
     $alumno_nota = new AlumnoNotas();
     $alumno_nota->id_alumno_control = $alumno_control['id_alumno_control'];
 
-
-    // $docentes =  new Users();
-    // $docentes->id_user = $alumno_control['id_user_docente'];
-    // $docente = $docentes->list_user();
-    // $docente = $alumno_control[0];
-
-    // $cursos =  new Cursos();
-    // $cursos->id_curso = $alumno_control['id_curso'];
-    // $curso = $cursos->list_curso();
-    // $curso = $curso[0];
     $curso =  $alumno_control['description'];
     ?> 
             <td><?php  echo  $alumno_control['last_name_docente'] ?></td>
@@ -123,93 +189,5 @@ foreach ($data_complete as $alumno_control) {
     </tbody>
 </table>
 <hr>
-
-<?php  ///weeek
-            $ddate = date('Y-m-d');
-            $date = new DateTime($ddate);
-            $week = $date->format("W");
-            // echo "Weeknummer: $week";
-            $add_sem = (is_numeric($_GET['week'])) ? $_GET['week'] : '0' ;
-            // if ($add_sem < 10 && $add_sem > - 10) {
-            //     $add_sem = $add_sem[0].'0'.$add_sem[1];
-
-            // }
-
-            $week_number = (date('W',strtotime( $add_sem." week") ) ) ;// (is_numeric($_GET['week'])  && $_GET['week'] != '0')? $_GET['week'] : $week ;
-            $year = date('Y');
-            for($day=1; $day<=7; $day++)
-            {
-                $fechas[] = "'".date('Y-m-d', strtotime($year."W".$week_number.$day))."'" ;
-            }
-
-            
-            // echo '<pre>'; var_dump( $fechas ); echo '</pre>';/***VAR_DUMP_DIE***/ 
- ?>
-
-<h2>Asistencias</h2>
-<h3>Alumno:XX</h3>
-<h4>Fecha: Del Lunes-13/02/2017   al   Domingo-20/02/2017 </h4>
-<a href="?week=<?php echo (  $add_sem - 1 ) ?>"> << Semena anterior</a>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<a href="?week=<?php echo ( $add_sem +  1 ) ?>">Semena siguiente >>  </a>
-<br>
-<br>
-
-
-<table class="pure-table">
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Docente</th>
-            <th>Curso</th>
-            <th>Lunes</th>
-            <th>Martes</th>
-            <th>Miércoles</th>
-            <th>Jueves</th>
-            <th>Viernes</th>
-            <th>Sábado</th>
-            <th>Domingo</th>
-        </tr>
-    </thead>
-
-    <tbody>
-
-            <?php 
-            foreach ($data_complete as $alumno_control) {
-            $alumno_asistencia =  new AlumnoAsistencias();
-            $alumno_asistencia->id_alumno_control = $alumno_control['id_alumno_control'];
-
-            $alumno_asistencia->string_ids = implode(',',$fechas);
-            $asistencia = $alumno_asistencia->list_alumno_control_asistencia();
-
-                // $docentes =  new Users();
-                // $docentes->id_user = $alumno_control['id_user_docente'];
-                // $docente = $docentes->list_user();
-                // $docente = $docente[0];
-
-                // $cursos =  new Cursos();
-                // $cursos->id_curso = $alumno_control['id_curso'];
-                // $curso = $cursos->list_curso();
-                $curso = $alumno_control['description']
-                ?> <tr class="pure-table-odd">
-                    <td> </td> 
-                    <td><?php echo  $alumno_control['last_name_docente'] ?> </td>                     
-                    <td><?php echo $curso ?> </td> 
-                    
-                
-                <?php  foreach ($asistencia as $dia) {    ?> 
-                            <?php foreach ($fechas as $fecha): ?>
-                                <?php if ($dia['fecha'] != $fecha) :  ?> 
-                                    <td></td>
-                                <?php  endif;  ?>
-                            <?php endforeach ?>
-                           <td><?php echo  $dia['asistencia'] ?></td>
-                <?php }            
-            ?> </tr>  <?php
-            }
-        ?>
-    </tbody>
-</table>
 
 </div>
